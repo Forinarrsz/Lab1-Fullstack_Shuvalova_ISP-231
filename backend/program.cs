4259f662bdf9efc3c49f1140e58ac0696c498e54
Forinarrsz <shuvalova0723@list.ru>
2026-01-16 11:23:46 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Shuvalova Alina Egorovna");
+Console.WriteLine(DateTime.Now);
+
